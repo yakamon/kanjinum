2591c43e9216a427d3cb0cd316a7c1ed2dd5dd9a
yakamon <p.m.byzance0817@gmail.com>
2020-05-23 18:55:55 +0900

    2020-05-23 18:55:55

--- convert.go
+++ convert.go
@@ -1,53 +1,58 @@
 package kanjinum
 
-import "math/big"
+import (
+	"math/big"
+)
 
 // NumToKanji converts int to string of kanji.
 func NumToKanji(n *big.Int) string {
-	digits := []string{"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"}
-	su := []string{"一", "十", "百", "千"}
-	lu := []string{"一", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議", "無量大数"}
+	D := []string{"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"}
+	S := []string{"", "十", "百", "千"}
+	L := []string{"", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議", "無量大数"}
 
+	// 符号
 	sign := "正ノ"
 	if n.Sign() < 0 {
 		sign = "負ノ"
 	}
-
-	zero, ten := big.NewInt(0), big.NewInt(10)
-	if n.CmpAbs(new(big.Int).Exp(ten, big.NewInt(68), nil)) >= 0 {
-		return sign + lu[len(lu)-1]
-	}
-
 	n.Abs(n)
 
-	s := ""
-	ndigit := 0
-
-	r := new(big.Int).Mod(n, ten).Int64()
-	n.Div(n, ten)
-	if r >= 1 {
-		s = digits[r] + s
-	} else if n.Cmp(zero) == 0 {
-		return digits[r]
+	zero, ten := big.NewInt(0), big.NewInt(10)
+	// 零
+	if n.Cmp(zero) == 0 {
+		return D[0]
 	}
-	ndigit++
+	// 無量大数
+	if n.Cmp(new(big.Int).Exp(ten, big.NewInt(68), nil)) >= 0 {
+		return sign + L[len(L)-1]
+	}
 
+	kanjinum := ""
+	part := ""
+	si := 0
+	li := 0
 	for n.Cmp(zero) > 0 {
-		r = new(big.Int).Mod(n, ten).Int64()
+		remain := new(big.Int).Mod(n, ten).Int64()
 		n.Div(n, ten)
-		if ndigit%4 == 0 {
-			s = lu[ndigit/4] + s
-			if r >= 1 {
-				s = digits[r] + s
-			}
-		} else if r > 0 {
-			s = su[ndigit%4] + s
-			if r >= 2 {
-				s = digits[r] + s
+		if remain > 0 {
+			part = S[si] + part
+			if si == 0 || remain > 1 {
+				part = D[remain] + part
 			}
 		}
-		ndigit++
-	}
+		si++
 
-	return sign + s
+		if si == 4 {
+			if len(part) > 0 {
+				kanjinum = part + L[li] + kanjinum
+			}
+			part = ""
+			si = 0
+			li++
+		}
+	}
+	if len(part) > 0 {
+		kanjinum = part + L[li] + kanjinum
+	}
+	return sign + kanjinum
 }
